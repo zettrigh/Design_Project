2364d40d0d28e5ce3e6f5bf31f43d7204a9ea4c6
zettrigh <efrempacheco78@gmail.com>
2026-07-06 22:15:17 -0400

    fix: agregar import faltante de Core\Router en index.php
    - index.php usaba new Router($/db) sin importar la clase Core\Router
      vía use, causando Class 'Router' not found en toda petición

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/bootstrap.php';
 use Config\Database;
 use Config\Environment;
 use Core\Middleware\SecurityHeadersMiddleware;
+use Core\Router;
 
 Environment::load(__DIR__ . '/.env');
 
